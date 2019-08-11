1e9f3ed58a9d6e7b46c1777a9214a1f61b90ddc8
p3lican <andyw504@gmail.com>
2019-08-11 17:15:36 -0500

    dist?

---
+++ dist/precache-manifest.875d6fc4946b6f534f97f4bf6da9d4dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "018798407cdc0cc9af6d",
+    "url": "/js/chunk-vendors.5f9bace1.js"
+  },
+  {
+    "revision": "9ca7fe02a77fba1d4ba9",
+    "url": "/js/app.65827ed5.js"
+  },
+  {
+    "revision": "24ec47adfeba0afcd343d73242a16271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72ec810f1cf3726573eddd5912912b6e",
+    "url": "/img/river-logo.72ec810f.png"
+  },
+  {
+    "revision": "9ca7fe02a77fba1d4ba9",
+    "url": "/css/app.e46b59c1.css"
+  }
+];

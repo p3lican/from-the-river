2d5699a2d543cbccc85a24fd54213c4411cb0103
p3lican <andyw504@gmail.com>
2019-08-11 23:49:39 -0500

    SDF

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,16 +4,6 @@
   <h2>Contact</h2>
   </div>
   <a href="mailto:andy@google.com">email.always.shows@even-offline.com</a>
-  <form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+
   </div>
 </template>

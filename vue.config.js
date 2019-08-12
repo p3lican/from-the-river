1ba58e577ccdac347bfc79d4684fa4666b56ccc3
p3lican <andyw504@gmail.com>
2019-08-11 20:31:21 -0500

    sdf

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ module.exports = {
           '/',
           '/about',
           '/contact',
+          '/engaging',
         ],
         captureAfterElementExists: '.videolist-wrapper',
       }),

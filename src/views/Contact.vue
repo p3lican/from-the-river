224ccc9abfe86141aaf1a1a544878fb90a8ab93a
p3lican <andyw504@gmail.com>
2019-09-28 14:15:37 -0500

    form maybe?

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,17 @@
 <template>
 <div class="videolist-wrapper">
-<div class="section-headline">
-  <h2>Contact</h2>
+  <div class="section-headline">
+    <h2>Contact</h2>
   </div>
   <a href="mailto:chamo.delosrios@gmail.com">chamo.delosrios@gmail.com</a>
-
+  <formz></formz>
   </div>
 </template>
 
 <script>
+import formz from '../components/formz.vue'
 export default {
+  components: {formz},
   metaInfo: {
     // title will be injected into parent titleTemplate
     title: 'Contact'
